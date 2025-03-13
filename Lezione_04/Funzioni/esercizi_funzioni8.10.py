caf9514154a37f3f94c29934dddfb17ed33bc1e5
Thomass055 <thomasmarchionni09@gmail.com>
2025-03-13 19:22:37 +0100

    save

---
+++ Lezione_04/Funzioni/esercizi_funzioni8.10.py
@@ -0,0 +1,13 @@
+lista_messaggi = ["Ciao","Come Stai","Tutto Bene?","A Presto!"]
+def send_messages(messaggi):
+
+    sent_messages=[]
+    upper_bound = len (messaggi)
+    print(f"{messaggi=}\n {sent_messages=}\n\n")
+    for i in range (upper_bound):
+        
+        message = messaggi.pop(0)
+        sent_messages.append(message)
+
+        print(f"{messaggi=}\n {sent_messages=}\n\n")
+send_messages(lista_messaggi)
---
+++ Lezione_04/Funzioni/esercizi_funzioni8.8.py
@@ -0,0 +1,10 @@
+def make_album(artist, title):
+    album:dict = {"artist": artist, "title": title}   
+    return album
+
+while True:
+    artist:str = str(input("Artist: "))
+    title:str = str(input("Title: "))
+
+    album = make_album(artist, title)
+    print(album)
