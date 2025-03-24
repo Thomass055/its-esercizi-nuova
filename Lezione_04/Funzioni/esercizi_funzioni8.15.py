1b63e4967f1ff881bbf6db719ee3b74ab2951c31
Thomass055 <thomasmarchionni09@gmail.com>
2025-03-24 19:28:42 +0100

    save

--- Lezione_04/Funzioni/esercizi_funzioni8.15.py
+++ Lezione_04/Funzioni/esercizi_funzioni8.15.py
@@ -1,1 +1,32 @@
 
+
+class Rectangle:
+
+
+
+    def __init__(self, x: float, y:float):
+
+        self.x: float = x
+
+        self.y: float = y
+
+
+
+    def area(self):
+
+        return self.x * self.y
+
+    
+
+    def __str__(self):
+
+        return f'Rectangle(x={self.x}, y={self.y})'
+
+    
+
+rect = Rectangle(2, 15)
+
+area = rect.area()
+
+print(f"L'area di {rect} è {area}")
+
---
+++ Lezione_06/Classi/esercizio9.1.py
@@ -0,0 +1,6 @@
+class Restaurant:
+    def __init__(self,Restaurant_name:str,cuisine_type:str):
+        self.Restaurant_name= Restaurant_name 
+        self.cuisine_type= cuisine_type
+        
+
